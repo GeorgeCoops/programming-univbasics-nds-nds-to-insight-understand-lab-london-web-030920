c6e3e0b2a82a19f7d76bcfb7091a2db4f057e942
George Cooper <coopsgeorge@gmail.com>
2020-03-05 15:50:36 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,29 +11,26 @@ end
 def print_first_directors_movie_titles
   inner_index = 0
   new_array = []
-  while inner_index <= vm[directors_database].length
+  while inner_index <= directors_database[inner_index].length
 
-  end
-  string = vending_machine[0][0][index][:title]
-  array.push(string)
-
-  while row_index < directors_database.length do
+    row_index = 0
     column_index = 0
     while column_index < vm[row_index].length do
       coord = "#{row_index}, #{column_index}"
       inner_len = vm[row_index][column_index].length
       # Remember \t is a TAB character for indentation
       puts "\tCoordinate [#{coord}] points to an #{vm[row_index][column_index].class} of length #{inner_len}"
-
+   
       inner_index = 0
       while inner_index < inner_len do
         puts "\t\t (#{coord}, #{inner_index}) is: #{vm[row_index][column_index][inner_index]}"
         inner_index += 1
       end
-
+   
       column_index += 1
     end
-
+   
     row_index += 1
-  end
+  end  
+  
 end
